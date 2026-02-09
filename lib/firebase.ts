6ca38f7df4bafeea0a267dba19448186ab0856ea
ImeshGimshan <iamcyklon@gmail.com>
2026-02-09 14:29:50 +0530

    feat: update Firebase to version 12.9.0 and refactor firebase.ts to use environment variables for configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzYTzJFWM3JWvz7uXRSjd364CItOk23E8",
-  authDomain: "imeshgimshan-89605.firebaseapp.com",
-  projectId: "imeshgimshan-89605",
-  storageBucket: "imeshgimshan-89605.firebasestorage.app",
-  messagingSenderId: "414230257232",
-  appId: "1:414230257232:web:690b462dd8c0eb2770bb55"
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET, 
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID, 
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
